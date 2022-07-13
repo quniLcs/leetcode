bcff5cd0c3b8e075c43bc2c9d390a9b847595c4e
Yiqun Wang <87119318+quniLcs@users.noreply.github.com>
2022-07-13 16:20:51 +0800

    Add files via upload

--- 1-two-sum.c
+++ 1-two-sum.c
@@ -9,9 +9,9 @@
 
 /* The following is a brutal solution
 int *twoSum(int *nums, int numsSize, int target, int *returnSize){
-    for (int i = 0; i < numsSize; i++) {
-        for (int j = i + 1; j < numsSize; j++) {
-            if (nums[i] + nums[j] == target) {
+    for (int i = 0; i < numsSize; i++){
+        for (int j = i + 1; j < numsSize; j++){
+            if (nums[i] + nums[j] == target){
                 int *ret = (int*)malloc(sizeof(int) * 2);
                 ret[0] = i;
                 ret[1] = j;
