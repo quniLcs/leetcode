ee789a84839a71933ceaf3c4031b71b5469b1a46
Yiqun Wang <87119318+quniLcs@users.noreply.github.com>
2022-08-02 14:58:49 +0800

    Add files via upload

---
+++ 53-maximum-subarray.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+
+int max(int a, int b){
+    return a > b ? a : b;
+}
+
+int maxSubArray(int* nums, int numsSize){
+    int pre = nums[0];
+    int ans = nums[0];
+    for (int index = 1; index < numsSize; index++){
+        pre = max(pre + nums[index], nums[index]);
+        ans = max(ans, pre);
+    }
+    return ans;
+}
+
+void main(){
+    int nums[] = {0, 0, -3, 1};
+    printf("%d\n", maxSubArray(nums, 4));
+}
