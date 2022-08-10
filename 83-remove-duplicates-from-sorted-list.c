107bfa966ca1d5ea860dc77839e9b22bfa0e2d17
Yiqun Wang <87119318+quniLcs@users.noreply.github.com>
2022-08-10 11:59:47 +0800

    Add files via upload

--- 83-remove-duplicates-from-sorted-list.c
+++ 83-remove-duplicates-from-sorted-list.c
@@ -6,4 +6,14 @@ struct ListNode{
 struct ListNode* deleteDuplicates(struct ListNode* head){
     struct ListNode *current = head;
 
+    while (current && current -> next){
+        if (current -> next -> val == current -> val){
+            current -> next = current -> next -> next;
+        }
+        else{
+            current = current -> next;
+        }
+    }
+
+    return head;
 }
