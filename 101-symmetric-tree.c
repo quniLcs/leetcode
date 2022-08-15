8700c58793ebbd2af5dbf1b2f6acbb0e9a224d63
Yiqun Wang <87119318+quniLcs@users.noreply.github.com>
2022-08-15 09:51:37 +0800

    Add files via upload

--- 101-symmetric-tree.c
+++ 101-symmetric-tree.c
@@ -1,13 +1,18 @@
+#include<stdio.h>
 #include<stdbool.h>
 
-struct TreeNode {
+struct TreeNode{
     int val;
     struct TreeNode *left;
     struct TreeNode *right;
 };
 
 bool AreSymmetric(struct TreeNode *p, struct TreeNode *q){
-    ;
+    if (p == NULL && q == NULL) return true;
+    else if (p == NULL && q != NULL) return false;
+    else if (p != NULL && q == NULL) return false;
+    else if (p -> val != q -> val) return false;
+    else return AreSymmetric(p -> left, q -> right) && AreSymmetric(p -> right, q -> left);
 }
 
 bool isSymmetric(struct TreeNode *root){
