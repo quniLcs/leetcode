75127b150e360b008dd4cd4f4979d323eb4678df
Yiqun Wang <87119318+quniLcs@users.noreply.github.com>
2022-08-17 11:42:45 +0800

    Add files via upload

---
+++ 112-path-sum.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+#include<stbool.h>
+
+struct TreeNode {
+    int val;
+    struct TreeNode *left;
+    struct TreeNode *right;
+};
+
+bool hasPathSum(struct TreeNode *root, int targetSum){
+    if (root == NULL){
+        return false;
+    }
+    else if (root -> left == NULL && root -> right == NULL){
+        return root -> val == targetSum;
+    }
+    else{
+        if (root -> left != NULL && hasPathSum(root -> left, targetSum - root -> val)) return true;
+        if (root -> right != NULL && hasPathSum(root -> right, targetSum - root -> val)) return true;
+        return false;
+    }
+}
