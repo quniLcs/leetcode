bcaf043b9e04cb81a2eecfef6c84a9c33a82f425
Yiqun Wang <87119318+quniLcs@users.noreply.github.com>
2022-08-18 10:42:44 +0800

    Add files via upload

---
+++ 115-distinct-subsequences.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+
+int numDistinct(char *s, char *t){
+    int m = strlen(s);
+    int n = strlen(t);
+
+    unsigned long long**dp = (unsigned long long**)malloc(sizeof(unsigned long long*) * (m + 1));
+    for (int i = 0; i <= m; i += 1){
+        dp[i] = (unsigned long long*)malloc(sizeof(unsigned long long) * (n + 1));
+        dp[i][0] = 1;
+    }
+    memset(dp[0] + 1, 0, sizeof(unsigned long long) * n);
+
+    for (int i = 1; i <= m; i += 1){
+        for (int j = 1; j <= n; j += 1){
+            if (s[i - 1] == t[j - 1]) dp[i][j] = dp[i - 1][j - 1] + dp[i - 1][j];
+            else dp[i][j] = dp[i - 1][j];
+        }
+    }
+
+    return dp[m][n];
+}
+
+void main(){
+    printf("%d\n", numDistinct("xslledayhxhadmctrliaxqpokyezcfhzaskeykchkmhpyjipxtsuljkwkovmvelvwxzwieeuqnjozrfwmzsylcwvsthnxujvrkszqwtglewkycikdaiocglwzukwovsghkhyidevhbgffoqkpabthmqihcfxxzdejletqjoxmwftlxfcxgxgvpperwbqvhxgsbbkmphyomtbjzdjhcrcsggleiczpbfjcgtpycpmrjnckslrwduqlccqmgrdhxolfjafmsrfdghnatexyanldrdpxvvgujsztuffoymrfteholgonuaqndinadtumnuhkboyzaqguwqijwxxszngextfcozpetyownmyneehdwqmtpjloztswmzzdzqhuoxrblppqvyvsqhnhryvqsqogpnlqfulurexdtovqpqkfxxnqykgscxaskmksivoazlducanrqxynxlgvwonalpsyddqmaemcrrwvrjmjjnygyebwtqxehrclwsxzylbqexnxjcgspeynlbmetlkacnnbhmaizbadynajpibepbuacggxrqavfnwpcwxbzxfymhjcslghmajrirqzjqxpgtgisfjreqrqabssobbadmtmdknmakdigjqyqcruujlwmfoagrckdwyiglviyyrekjealvvigiesnvuumxgsveadrxlpwetioxibtdjblowblqvzpbrmhupyrdophjxvhgzclidzybajuxllacyhyphssvhcffxonysahvzhzbttyeeyiefhunbokiqrpqfcoxdxvefugapeevdoakxwzykmhbdytjbhigffkmbqmqxsoaiomgmmgwapzdosorcxxhejvgajyzdmzlcntqbapbpofdjtulstuzdrffafedufqwsknumcxbschdybosxkrabyfdejgyozwillcxpcaiehlelczioskqtptzaczobvyojdlyflilvwqgyrqmjaeepydrcchfyftjighntqzoo", "rwmimatmhydhbujebqehjprrwfkoebcxxqfktayaaeheys"));
+}
